65adfae239fdea63cdbf80c2a5a0e68dba24f87e
bhanusri2 <140478066+bhanusri2@users.noreply.github.com>
2024-03-17 12:33:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Pairs whose sum is equal to X/count-pairs-whose-sum-is-equal-to-x.py
@@ -0,0 +1,95 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+    
+        
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def countPair(self, head1, head2, n1, n2, x):
+        '''
+        head1:  head of linkedList 1
+        head2:  head of linkedList 2
+        n1:  len of  linkedList 1
+        n2:  len of linkedList 1
+        x:   given sum
+        
+        '''
+        def link_list(head):
+            current = head
+            result = []
+            while current is not None:
+                result.append(current.data)
+                current = current.next
+            return result
+        
+        l1=link_list(head1)
+        
+        c=0
+        l2=link_list(head2)
+        l2_set = set(l2)
+        count = 0
+    
+        for num in l1:
+            complement = x - num
+            if complement in l2_set:
+                count += 1
+    
+        return count
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        
+        n1=int(input())
+        ll1 = LinkedList() # create a new linked list 'll1'.
+        nodes_ll1 = list(map(int, input().strip().split()))
+        for nodes in nodes_ll1:
+            ll1.append(nodes)  # add to the end of the list
+        
+        n2=int(input())
+        ll2=LinkedList()  #create a new linked list 'll1'.
+        nodes_ll2 = list(map(int, input().strip().split()))
+        for nodes in nodes_ll2:
+            ll2.append(nodes)  # add to the end of the list
+        
+        x=int(input())
+        
+        
+        print(Solution().countPair(ll1.head,ll2.head,n1,n2,x))
+
+
+# } Driver Code Ends
